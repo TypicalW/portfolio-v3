ed4dcf40ffb1035ba53f95f5f73b3aceefc8f428
TypicalW <ashmit1665@gmail.com>
2026-08-26 23:37:32 +0530

    Added Socials

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ from flask import (
 app = Flask(__name__)
 
 
+
+SOCIALS = {
+    "github": "https://github.com/TypicalW",
+    "linkedin": "https://www.linkedin.com/in/ashmitmishra/",
+    "x": "https://x.com/Typical__W",
+    "codeforces": "https://codeforces.com/profile/TypicalW",
+}
+
+
+
+@app.context_processor
+def inject_globals():
+    return {
+        "socials": SOCIALS,
+    }
+
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -19,8 +37,6 @@ def about():
 @app.route("/projects/")
 def projects():
     return render_template("projects.html")
-
-
 
 
 
